fb8440cf83a7a8021cd1a3b969ffbf1e3a01a5ca
Sewens <Sewen@mail.dlut.edu.cn>
2023-08-15 19:18:22 +0800

    complets quiz3

--- Homework1/Execise.hs
+++ Homework1/Execise.hs
@@ -1,5 +1,6 @@
 {-
 Execise1
+Integer to list and reverse order list
 -}
 toDigits :: Integer -> [Integer]
 toDigits x
@@ -13,9 +14,23 @@ toDigitsRev x
 
 {-
 Execise2
+Double element on even index
 -}
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther xs
         | null xs = []
         |otherwise = [if even i then x*2 else x | (i,x) <- listWithIndex]
             where listWithIndex = zip [0..] xs
+
+{-
+Execise3
+Sum Integer List
+-}
+sumOfDigits :: [Integer] -> Integer
+-- sumOfDigits = foldl (+) 0
+-- sumOfDigits = sum
+
+sumOfDigits (x:xs)
+        | null (x:xs) = 0     -- null list
+        | null xs = x           --list with one elem
+        | otherwise = x + sumOfDigits xs
