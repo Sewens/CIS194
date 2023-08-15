9824fc58b4521dd14bffd8b4d6cebaf0537b4331
Sewens <Sewen@mail.dlut.edu.cn>
2023-08-15 18:37:20 +0800

    优化表达，by hlint

--- Homework1/Execise1.hs
+++ Homework1/Execise1.hs
@@ -5,9 +5,9 @@ Homework1 by Sewen.
 toDigits :: Integer -> [Integer]
 toDigits x
         | x <=0 = []
-        | otherwise = (toDigits $ x`div`10) ++ [x`mod`10]
+        | otherwise = toDigits (x `div` 10) ++ [x `mod` 10]
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev x
         | x <=0 = []
-        | otherwise = x`mod`10 : (toDigitsRev $ x`div`10)
+        | otherwise = x`mod`10 : toDigitsRev (x`div`10)
